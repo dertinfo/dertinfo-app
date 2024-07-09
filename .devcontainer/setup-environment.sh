b59166a41473826b39ac635886b4986ebf604e17
David Hall <davidsmonkeys@hotmail.com>
2024-07-09 11:43:06 +0100

    fix: revert to isolation of the envionment setup from the devcontainer setup

--- .devcontainer/setup-environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install global npm packages
-npm install -g typescript @azure/static-web-apps-cli azure-functions-core-tools@3 @angular/cli@13
-
-# Append a message to codespace.md
-echo '# Writing code upon codespace creation!' >> codespace.md
